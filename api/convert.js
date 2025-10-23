36d32b680c1f537069137fbe89ac4c21cda49217
Charmie Rafols <charmierafols@gmail.com>
2025-10-23 16:23:06 +0800

    Added comments.

--- api/convert.js
+++ api/convert.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     const { from, to } = req.query;
 
-    const apiKey = process.env.CURRENCY_API_KEY; // get api key from .env file
+    const apiKey = process.env.CURRENCY_API_KEY; // get api key from .env file from Vercel
 
     try {
         const url = `https://api.currencyapi.com/v3/latest?apikey=${apiKey}&base_currency=${from}&currencies=${to}`; // construct the API URL
